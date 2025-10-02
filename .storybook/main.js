ae9ea472528198ebc4aefd4cb741e596bcfcb902
Tim Perry <pimterry@gmail.com>
2025-10-02 16:55:29 +0200

    Migrate to Vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|tsx)'],
   addons: ['@storybook/addon-docs'],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
@@ -12,29 +12,14 @@ module.exports = {
     check: true,
     reactDocgen: false,
   },
-  webpackFinal: async (config) => {
-    const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+  async viteFinal(config) {
+    const { default: checker } = await import('vite-plugin-checker');
 
-    // Ensure babel-loader processes story files
-    config.module.rules.push({
-      test: /\.(ts|tsx|js|jsx)$/,
-      include: /stories/,
-      use: [{
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [
-            [require.resolve('@babel/preset-react'), { runtime: 'automatic' }],
-            require.resolve('@babel/preset-typescript')
-          ]
-        }
-      }]
-    });
-
-    // Add TypeScript type checking
+    config.plugins = config.plugins || [];
     config.plugins.push(
-      new ForkTsCheckerWebpackPlugin({
+      checker({
         typescript: {
-          configFile: require('path').resolve(__dirname, '../stories/tsconfig.json'),
+          tsconfigPath: './stories/tsconfig.json',
         },
       })
     );
